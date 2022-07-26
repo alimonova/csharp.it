1205c26440bed9f6971cb5e7dc669d26e8694e67
Admin <admin@MacBook-Pro-Admin.local>
2022-07-26 16:43:16 +0300

    Mappings were added

---
+++ csharp.it/Mappings/ApplicationMapping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using csharp.it.Dto;
+using csharp.it.Models;
+
+namespace csharp.it.Mappings
+{
+    public class ApplicationMapping : AutoMapper.Profile
+    {
+        public ApplicationMapping()
+        {
+            CreateMap<Answer, AnswerDto>().ReverseMap();
+            CreateMap<Chapter, ChapterDto>().ReverseMap();
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Group, GroupDto>().ReverseMap();
+            CreateMap<Lesson, LessonDto>().ReverseMap();
+            CreateMap<PracticalExample, PracticalExampleDto>().ReverseMap();
+            CreateMap<Question, QuestionDto>().ReverseMap();
+            CreateMap<Tarif, TarifDto>().ReverseMap();
+            CreateMap<Task, TaskDto>().ReverseMap();
+            CreateMap<UsefulResource, UsefulResourceDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<UserGroup, UserGroupDto>().ReverseMap();
+            CreateMap<UserTask, UserTaskDto>().ReverseMap();
+        }
+    }
+}
+
