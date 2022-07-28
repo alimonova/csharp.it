b1722903b6af53a0e4e2ce8be73bf65bb14cc777
Admin <admin@MacBook-Pro-Admin.local>
2022-07-28 23:14:15 +0300

    Account functionality was refactored

--- csharp.it/Controllers/AccountController.cs
+++ csharp.it/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using csharp.it.Dto;
 using csharp.it.Models;
+using csharp.it.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -22,42 +23,28 @@ namespace csharp.it.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly UserManager<User> _userManager;
-        private readonly DbContext _context;
         private readonly IMapper _mapper;
-        readonly ILogger<AccountController> _log;
-        private readonly TokenSettings _appSettings;
-        private readonly IConfiguration _configuration;
+        private readonly IAccountService _service;
 
-        public AccountController(UserManager<User> userManager, IMapper mapper,
-            DbContext context, ILogger<AccountController> log,
-            IOptions<TokenSettings> appSettings,
-            IConfiguration configuration)
+
+        public AccountController(IMapper mapper, IAccountService service)
         {
-            _context = context;
-            _userManager = userManager;
             _mapper = mapper;
-            _log = log;
-            _appSettings = appSettings.Value;
-            _configuration = configuration;
+            _service = service;
         }
 
         [HttpPost("Registration")]
-        public async Task<IActionResult> Register(string Email, string FirstName,
-            string LastName, string Password)
+        public async Task<IActionResult> Register(UserRegistration model)
         {
             if (ModelState.IsValid)
             {
-                var user = new User { Email = Email, UserName = Email,
-                    FirstName = FirstName, LastName = LastName };
-                var result = await _userManager.CreateAsync(user, Password);
-                _context.SaveChanges();
+                var result = await _service.Register(model);
                 
                 if (!result.Succeeded)
                 {
                     return BadRequest(result.Errors);
                 }
-                await _userManager.AddToRoleAsync(user, "student");
+
                 return Created("", "Пользователь успешно создан.");
             }
             else
@@ -68,97 +55,60 @@ namespace csharp.it.Controllers
 
         [Authorize(Roles = "ADMIN")]
         [HttpGet("Teachers")]
-        public async Task<IActionResult> GetTeachers()
+        public async Task<ActionResult<UserDto>> GetTeachers()
         {
-            var teachers = await _userManager.GetUsersInRoleAsync("Teacher");
+            var teachers = await _service.GetUsersOfRole("TEACHER");
             var _teachers = _mapper.Map<IEnumerable<UserDto>>(teachers);
-            return Ok(teachers);
+            return Ok(_teachers);
         }
 
-        [Authorize]
+        [Authorize(Roles = "ADMIN, TEACHER")]
         [HttpGet("Students")]
-        public async Task<IActionResult> GetStudents()
+        public async Task<ActionResult<UserDto>> GetStudents()
         {
-            var students = await _userManager.GetUsersInRoleAsync("Student");
+            var students = await _service.GetUsersOfRole("STUDENT");
             var _students = _mapper.Map<IEnumerable<UserDto>>(students);
             return Ok(_students);
         }
 
-        [HttpPost("Authorization")]
-        public async Task<IActionResult> Authenticate([FromBody] UserAuthorization model)
+        [HttpPost("Authentication")]
+        public async Task<IActionResult> Authenticate(UserAuthorization model)
         {
+            var jwt = await _service.Authenticate(model);
 
-            var users = _context.Users.Where(x => x.Email == model.Email).ToList();
-            User user = null;
-
-            if (users.Count() > 0)
+            if (jwt == null)
             {
-                user = users[0];
-            }
-
-            if (user == null ||
-                !await this._userManager.CheckPasswordAsync(user, model.Password))
-            {
-                _log.LogWarning($"AUTHORIZATION: User was not logged in " +
-                    $"(wrong password). Returned unauthorized.");
                 return Unauthorized();
             }
 
-            var roleId = _context.UserRoles.First(x => x.UserId == user.Id).RoleId;
-            var role = _context.Roles.First(x => x.Id == roleId).Name;
-
-            var claims = new List<Claim>();
-            claims.Add(new Claim("UserID", user.Id.ToString()));
-            claims.Add(new Claim(ClaimTypes.Role, role));
-
-            var now = DateTime.UtcNow;
-            var jwt = new JwtSecurityToken(
-                    issuer: _appSettings.Issuer,
-                    audience: _appSettings.Audience,
-                    notBefore: now,
-                    claims: claims,
-                    expires: now.Add(TimeSpan.FromMinutes(_appSettings.Lifetime)),
-                    signingCredentials: new SigningCredentials(_appSettings.
-                    GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
             var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
             return Ok(new
             {
                 Token = encodedJwt,
-                Id = user.Id,
-                UserName = user.UserName,
                 ValidTo = jwt.ValidTo
             });
         }
 
-        [HttpGet("Roles")]
+        [Authorize]
+        [HttpGet("MyRoles")]
         public async Task<IActionResult> GetRoles()
         {
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-            IEnumerable<Claim> claims = identity.Claims;
-
-            if (claims.Count() != 0)
-            {
-                var id = Guid.Parse(claims.First(x => x.Type == "UserID").Value);
-
-                IList<string> roles = new List<string> { "Роль не определена" };
-                User user = _context.Users.First(x => x.Id == id);
-                if (user != null)
-                    roles = await _userManager.GetRolesAsync(user);
-                return Ok(roles);
-            }
-
-            return Unauthorized();
+            var roles = await _service.GetCurrentUserRoles();
+            return Ok(roles);
         }
 
         [Authorize(Roles = "ADMIN")]
         [HttpGet("Roles/{email}")]
         public async Task<IActionResult> GetRolesByEmail(string email)
         {
-            IList<string> roles = new List<string> { "Роль не определена" };
-            User user = _context.Users.First(x => x.Email == email);
-            if (user != null)
-                roles = await _userManager.GetRolesAsync(user);
+            var roles = await _service.GetRolesByUserEmail(email);
+
+            if (roles == null)
+            {
+                return BadRequest();
+            }
+
             return Ok(roles);
         }
     }
